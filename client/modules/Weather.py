e8d926d154df783041c3524204108058581e33f4
computercolinx <computercolinx@gmail.com>
2016-01-25 20:58:59 -0500

    fixed time

--- client/modules/Weather.py
+++ client/modules/Weather.py
@@ -25,7 +25,7 @@ def handle(text, mic, profile):
         forecast_time = datetime.datetime.combine(datetime.datetime.now().date(),datetime.time(hour=21))
     elif bool(re.search(r'\b(tomorrow)\b', text, re.IGNORECASE)):
         forecast_time = datetime.datetime.combine(datetime.datetime.now().date(),datetime.time(hour=12))
-        forecast_time = time + datetime.timedelta(days=1)
+        forecast_time = forecast_time + datetime.timedelta(days=1)
     forecast = forecastio.load_forecast(api_key,lat, long,time=forecast_time)
     mic.say(forecast.currently().summary)
     
